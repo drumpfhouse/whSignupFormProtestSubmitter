cfb0b66263669cb4fe957f669fb1d75add5dbf81
DrumpfHouse <drumpfhouse@gmail.com>
2017-02-08 20:27:33 -0500

    Protest submitter: Now submitting to more forms!

--- whSignupFormProtestSubmitter.py
+++ whSignupFormProtestSubmitter.py
@@ -4,6 +4,82 @@ import random
 import sys
 import time
 
+def _buildName():
+    fnames = [
+        'Abraham',
+        'Andrew',
+        'Barack',
+        'Benjamin',
+        'Calvin',
+        'Chester',
+        'Dwight',
+        'Franklin',
+        'George',
+        'Gerald',
+        'Grover',
+        'Harry',
+        'Herbert',
+        'James',
+        'John',
+        'Lyndon',
+        'Martin',
+        'Millard',
+        'Richard',
+        'Ronald',
+        'Rutherford',
+        'Theodore',
+        'Thomas',
+        'Ulysses',
+        'Warren',
+        'William',
+        'Woodrow',
+        'Zachary'
+    ]
+
+    lnames = [
+        'Adams',
+        'Arthur',
+        'Buchanan',
+        'Bush',
+        'Carter',
+        'Cleveland',
+        'Clinton',
+        'Coolidge',
+        'Eisenhower',
+        'Fillmore',
+        'Ford',
+        'Garfield',
+        'Grant',
+        'Harding',
+        'Harrison',
+        'Hayes',
+        'Hoover',
+        'Jackson',
+        'Jefferson',
+        'Johnson',
+        'Kennedy',
+        'Knox',
+        'Lincoln',
+        'Madison',
+        'McKinley',
+        'Monroe',
+        'Nixon',
+        'Obama',
+        'Pierce',
+        'Reagan',
+        'Roosevelt',
+        'Taft',
+        'Taylor',
+        'Truman',
+        'Tyler',
+        'VanBuren',
+        'Washington',
+        'Wilson'
+    ]
+
+    return (random.choice(fnames), random.choice(lnames))
+
+
 def _buildFakeEmailAddress():
     domains = [
         'trump.com',
@@ -46,6 +122,16 @@ def _buildFakeEmailAddress():
         'yopmail.com',
         'zippymail.info',
         'zxcvbnm.co.uk',
+        'whitehouse.gov',
+        'state.gov',
+        'irs.gov',
+        'epa.gov',
+        'gop.com',
+        'army.mil',
+        'navy.mil',
+        'af.mil',
+        'congress.gov',
+        'senate.gov'
         ]
 
     localParts = [
@@ -62,41 +148,95 @@ def _buildFakeEmailAddress():
         'giantmeteor2016',
         'freescience',
         'putinitinyou',
-        'ivankadrinkyerblood',
+        'ivankadanke',
         'SN.atch.grabber',
         'drumpfhouse',
         're.fugee',
         'australasia',
         'youcrane',
         'gldnshwrs',
-        'factsschmacts'
+        'factsschmacts',
+        'smoochinmnuchin',
+        'mike.penceive',
+        'gorsucks',
+        'de.voss',
+        'donaldtrumpmakesmewannasmokecrack'
     ]
 
     return str.join('', [random.choice(localParts), '@', random.choice(domains)])
 
 
-def sendProtestSubmission(email):
+def frontPageForm():
+        targetURL = 'https://forms.whitehouse.gov/webform/email-signup'
 
-    targetURL = 'https://forms.whitehouse.gov/webform/email-signup'
+        dataPayload = {
+            "submitted[email_address]": _buildFakeEmailAddress(),
+            "submitted[zip_code]": "12345",
+            "form_id": "webform_client_form_111",
+            "form_build_id": "form-43X7sWhYGJ1EdVKeroNYk0M2Wnv7I-Bp4qrOtulPg6A"
+        }
+
+        return {'targetURL': targetURL, 'dataPayload': dataPayload}
+
+def gorsuchForm():
+    targetURL = 'https://forms.whitehouse.gov/webform/scotus-form'
+
+    fname, lname = _buildName()
 
     dataPayload = {
-        "submitted[email_address]": email,
+        "submitted[first_name]": fname,
+        "submitted[last_name]": lname,
+        "submitted[e_mail_address]": _buildFakeEmailAddress(),
         "submitted[zip_code]": "12345",
-        "form_id": "webform_client_form_111",
-        "form_build_id": "form-43X7sWhYGJ1EdVKeroNYk0M2Wnv7I-Bp4qrOtulPg6A"
+        "form_id": "webform_client_form_106",
+        "form_build_id": "form-sZV-iGQZ-ZjG8D9H_5SGIZfBSBEsGfiLx-mjVrXt20E"
     }
 
-    r = requests.post(targetURL, data=dataPayload)
+    return {'targetURL': targetURL, 'dataPayload': dataPayload}
+
+def workForm():
+    targetURL = 'https://forms.whitehouse.gov/webform/get-involved'
+
+    fname, lname = _buildName()
+
+    dataPayload = {
+        "submitted[first_name]": fname,
+        "submitted[last_name]": lname,
+        "submitted[email_address]": _buildFakeEmailAddress(),
+        "submitted[zip_code]": "12345",
+        "form_id": "webform_client_form_141",
+        "form_build_id": "form-6kxJzAO-R2p9ejec8AywNsveIW9AnRlHbM1v19Gp2Ug"
+    }
+
+    return {'targetURL': targetURL, 'dataPayload': dataPayload}
+
+
+def sendProtestSubmission(form):
+
+    data = form()
+    try:
+        r = requests.post(data['targetURL'], data=data['dataPayload'])
+    except Exception as err:
+        print("Woops.  Something went sideways. " + err)
+
     return r.status_code
 
 
 def fireTehLazers(iterations=5):
 
     for i in range(0,iterations):
-        email = _buildFakeEmailAddress()
-        result = sendProtestSubmission(email)
-        sleepTime = random.randint(1,30)
-        print(str(i + 1) + " of " + str(iterations) + ": " + email + ", result code: " + str(result) + ". Next post in " + str(sleepTime) + " seconds.")
+
+        forms = [
+            frontPageForm,
+            gorsuchForm,
+            workForm
+        ]
+
+        form = random.choice(forms)
+
+        result = sendProtestSubmission(form)
+        sleepTime = random.randint(1,10)
+        print(str(i + 1) + " of " + str(iterations) + ": " + form.__name__ + ", result code: " + str(result) + ". Next post in " + str(sleepTime) + " seconds.")
         time.sleep(sleepTime)
 
 
