6367afc29652460d93158e42ffbc2f8eed548ecf
DrumpfHouse <drumpfhouse@gmail.com>
2017-02-18 09:48:46 -0500

    Adding targets for the new "stands with israel" and "empowering women" pseudo-petitions.

--- whSignupFormProtestSubmitter.py
+++ whSignupFormProtestSubmitter.py
@@ -4,6 +4,7 @@ import random
 import sys
 import time
 
+
 def _buildName():
     fnames = [
         'Abraham',
@@ -83,8 +84,8 @@ def _buildName():
 def _buildZipCode():
     zipDigits = []
 
-    for i in range(0,5):
-        zipDigits.append(str(random.randint(0,9)))
+    for i in range(0, 5):
+        zipDigits.append(str(random.randint(0, 9)))
 
     return ''.join(zipDigits)
 
@@ -141,7 +142,7 @@ def _buildFakeEmailAddress():
         'af.mil',
         'congress.gov',
         'senate.gov'
-        ]
+    ]
 
     localParts = [
         'dumptrump',
@@ -169,23 +170,25 @@ def _buildFakeEmailAddress():
         'mike.penceive',
         'gorsucks',
         'de.voss',
-        'donaldtrumpmakesmewannasmokecrack'
+        'donaldtrumpmakesmewannasmokecrack',
+        'formsarenotpetitions'
     ]
 
     return str.join('', [random.choice(localParts), '@', random.choice(domains)])
 
 
 def frontPageForm():
-        targetURL = 'https://forms.whitehouse.gov/webform/email-signup'
+    targetURL = 'https://forms.whitehouse.gov/webform/email-signup'
 
-        dataPayload = {
-            "submitted[email_address]": _buildFakeEmailAddress(),
-            "submitted[zip_code]": _buildZipCode(),
-            "form_id": "webform_client_form_111",
-            "form_build_id": "form-43X7sWhYGJ1EdVKeroNYk0M2Wnv7I-Bp4qrOtulPg6A"
-        }
+    dataPayload = {
+        "submitted[email_address]": _buildFakeEmailAddress(),
+        "submitted[zip_code]": _buildZipCode(),
+        "form_id": "webform_client_form_111",
+        "form_build_id": "form-43X7sWhYGJ1EdVKeroNYk0M2Wnv7I-Bp4qrOtulPg6A"
+    }
 
-        return {'targetURL': targetURL, 'dataPayload': dataPayload}
+    return {'targetURL': targetURL, 'dataPayload': dataPayload}
+
 
 def gorsuchForm():
     targetURL = 'https://forms.whitehouse.gov/webform/scotus-form'
@@ -199,6 +202,40 @@ def gorsuchForm():
         "submitted[zip_code]": _buildZipCode(),
         "form_id": "webform_client_form_106",
         "form_build_id": "form-sZV-iGQZ-ZjG8D9H_5SGIZfBSBEsGfiLx-mjVrXt20E"
+    }
+
+    return {'targetURL': targetURL, 'dataPayload': dataPayload}
+
+
+def israelForm():
+    targetURL = 'https://forms.whitehouse.gov/webform/trump-stands-with-israel'
+
+    fname, lname = _buildName()
+
+    dataPayload = {
+        "submitted[first_name]": fname,
+        "submitted[last_name]": lname,
+        "submitted[e_mail_address]": _buildFakeEmailAddress(),
+        "submitted[zip_code]": _buildZipCode(),
+        "form_id": "webform_client_form_176",
+        "form_build_id": "form-zsH6jltzBOSHwMFO5DyO0Ki9DSVWIjxVSxLydxlsSd0"
+    }
+
+    return {'targetURL': targetURL, 'dataPayload': dataPayload}
+
+
+def womenForm():
+    targetURL = 'https://forms.whitehouse.gov/webform/empowering-female-leaders'
+
+    fname, lname = _buildName()
+
+    dataPayload = {
+        "submitted[first_name]": fname,
+        "submitted[last_name]": lname,
+        "submitted[e_mail_address]": _buildFakeEmailAddress(),
+        "submitted[zip_code]": _buildZipCode(),
+        "form_id": "webform_client_form_166",
+        "form_build_id": "form-NqsMkzEZaaDlyQrTXOKzDh1K-R60re0e1pglFMgWIR4"
     }
 
     return {'targetURL': targetURL, 'dataPayload': dataPayload}
@@ -221,7 +258,6 @@ def workForm():
 
 
 def sendProtestSubmission(form):
-
     data = form()
     try:
         r = requests.post(data['targetURL'], data=data['dataPayload'])
@@ -232,20 +268,21 @@ def sendProtestSubmission(form):
 
 
 def fireTehLazers(iterations=5):
-
-    for i in range(0,iterations):
-
+    for i in range(0, iterations):
         forms = [
             frontPageForm,
             gorsuchForm,
-            workForm
+            workForm,
+            israelForm,
+            womenForm
         ]
 
         form = random.choice(forms)
 
         result = sendProtestSubmission(form)
-        sleepTime = random.randint(1,10)
-        print(str(i + 1) + " of " + str(iterations) + ": " + form.__name__ + ", result code: " + str(result) + ". Next post in " + str(sleepTime) + " seconds.")
+        sleepTime = random.randint(1, 10)
+        print(str(i + 1) + " of " + str(iterations) + ": " + form.__name__ + ", result code: " + str(
+            result) + ". Next post in " + str(sleepTime) + " seconds.")
         time.sleep(sleepTime)
 
 
@@ -256,12 +293,13 @@ def main(args):
         print('whSignupFormProtestSubmitter.py -i <number_of_submissions>')
         sys.exit(2)
     for opt, arg in opts:
-        if opt in ("-i", "--iterations") and isinstance(int(arg), int ):
+        if opt in ("-i", "--iterations") and isinstance(int(arg), int):
             iterations = int(arg)
             fireTehLazers(iterations)
         else:
             print('whSignupFormProtestSubmitter.py -i <number_of_submissions>')
             sys.exit(2)
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
